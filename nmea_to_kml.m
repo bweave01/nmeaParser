7caa9ad7ce5ba2e7573152ecd9c2af17b9a918f7
Brandon Weaver <brandon.weaver@tufts.edu>
2019-09-27 11:05:52 -0400

    Modified the output KML file to match with KML file output from SiRFDemo

--- nmea_to_kml.m
+++ nmea_to_kml.m
@@ -20,32 +20,26 @@ fid = fopen(kml_name, 'wt');
 
 % Write headers to KML file
 line_1 = '<?xml version="1.0" encoding="UTF-8"?>\n';
-line_2 = '<kml xmlns="http://www.opengis.net/kml/2.2">\n';
+line_2 = '<kml xmlns="http://earth.google.com/kml/2.1">\n';
 line_3 = '<Document>\n';
+line_4 = '  <Style id="red"><IconStyle><color>ff0000ff</color><scale>.3</scale></IconStyle></Style>\n';
+line_5 = '  <Style id="green"><IconStyle><color>ff00ff00</color><scale>.3</scale></IconStyle></Style>\n';
+line_6 = '  <Style id="blue"><IconStyle><color>ffff0000</color><scale>.3</scale></IconStyle></Style>\n';
+line_7 = '  <Style id="cyan"><IconStyle><color>ffff41b0</color><scale>.3</scale></IconStyle></Style>\n';
 fprintf(fid,line_1);
 fprintf(fid,line_2);
 fprintf(fid,line_3);
+fprintf(fid,line_4);
+fprintf(fid,line_5);
+fprintf(fid,line_6);
+fprintf(fid,line_7);
 
 % Write Placemarks to KML file
 num_placemarks = length(nmea.gga.utc_time);
 for i = 1:num_placemarks
-    deg_lat = str2num(nmea.gga.latitude(i,1:2));
-    min_lat = str2double(nmea.gga.latitude(i,3:end));
-    lat = deg_lat + (min_lat/60);
     
-    if nmea.gga.n_s_indic(i,1) == 'S'
-        lat = lat*-1;
-    end
+    write_placemark(fid,nmea,i);  
     
-    deg_lon = str2num(nmea.gga.longitude(i,1:3));
-    min_lon = str2double(nmea.gga.longitude(i,4:end));
-    lon = deg_lon + (min_lon/60);
-    
-    if nmea.gga.e_w_indic(i,1) == 'W'
-        lon = lon*-1;
-    end
-    
-    write_placemark(fid,lon,lat);  
 end
 
 fprintf(fid,'</Document>');
--- write_placemark.m
+++ write_placemark.m
@@ -1,11 +1,56 @@
-function write_placemark(fid,lon,lat)
+function write_placemark(fid,nmea,ind)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
+% Latitude and Longitude
+deg_lat = str2num(nmea.gga.latitude(ind,1:2));
+min_lat = str2double(nmea.gga.latitude(ind,3:end));
+lat = deg_lat + (min_lat/60);
+    
+if nmea.gga.n_s_indic(ind,1) == 'S'
+    lat = lat*-1;
+end
+
+deg_lon = str2num(nmea.gga.longitude(ind,1:3));
+min_lon = str2double(nmea.gga.longitude(ind,4:end));
+lon = deg_lon + (min_lon/60);
+
+if nmea.gga.e_w_indic(ind,1) == 'W'
+    lon = lon*-1;
+end
+
+lat_lon_str = sprintf(' Lat = %.7f deg<br/> Lon = %.7f deg<br/>',lat,lon);
+
+% UTC time
+hh = str2num(nmea.gga.utc_time(ind,1:2));
+mm = str2num(nmea.gga.utc_time(ind,3:4));
+ss = str2double(nmea.gga.utc_time(ind,5:end));
+utc_str = sprintf(' UTC = %d:%d:%6.3f<br/>',hh,mm,ss);
+
+% SV count, altitude, HDOP
+sv_count = str2num(nmea.gga.sats_used(ind,:));
+
+altitude = str2double(nmea.gga.msl_altitude(ind,:));
+if nmea.gga.units_alt(ind,1) == 'M'
+    alt_units = 'm';
+else
+    alt_units = 'units';
+end
+
+hdop = str2double(nmea.gga.hdop(ind,:));
+
+sv_count_str = sprintf(' SV Cnt = %d<br/>', sv_count);
+alt_str = sprintf(' Alt (msl)=%.2f ', altitude);
+alt_str = [alt_str,alt_units,'<br/>'];
+hdop_str = sprintf(' Hdop = %.2f<br/>',hdop);
+
+% Writing Placemark to file
 fprintf(fid,'  <Placemark>\n    <name></name>\n');
-fprintf(fid,'    <description></description>\n');
+description = ['GPS Quality Indicator = 1<br/>',utc_str,sv_count_str,lat_lon_str,alt_str,hdop_str];
+fprintf(fid,'    <description>%s</description>\n',description);
+fprintf(fid,'    <styleUrl>red</styleUrl>\n');
 fprintf(fid,'    <Point>\n      <coordinates>');
-fprintf(fid,'%.13f,%.14f,0</coordinates>\n',lon,lat);
+fprintf(fid,'%.7f,%.7f,0</coordinates>\n',lon,lat);
 fprintf(fid,'    </Point>\n  </Placemark>\n');
 
 end
